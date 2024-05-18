98ce8ba1c6ee6ba9062318bb52ffacdef918316a
yaseeen96 <syedyaseen1539@gmail.com>
2024-05-18 09:56:34 +0530

    first commit in frontend branch

--- frontend application/lib/main.dart
+++ frontend application/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'ST STARTER TEMPLATE', // change the title
+      title: 'Hyd Isthma App', // change the title
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
