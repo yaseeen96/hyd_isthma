925d15e6339d2cdf4a0bf4a073f4b3329dc5e729
yaseeen96 <syedyaseen1539@gmail.com>
2024-06-10 10:24:25 +0530

    add check token validity logic

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -1,7 +1,38 @@
-
-
+import { ToastContainer } from 'react-toastify';
+import { useDarkMode } from './utils/hooks/useDarkMode';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
+import useAsyncEffect from 'use-async-effect';
+import { isUserLoggedIn } from './services/check_token_validity_service';
 function App({ children }) {
-    return <div className="main-section antialiased relative font-nunito text-sm font-normal">{children}</div>;
+    const navigate = useNavigate();
+    const isDarkMode = useDarkMode();
+    useAsyncEffect(async () => {
+        const { user, isLoggedIn } = await isUserLoggedIn();
+        if (isLoggedIn) {
+            localStorage.setItem('name', user.name);
+            localStorage.setItem('arrivalConfirmed', user.confirm_arrival);
+        } else {
+            navigate('/login');
+        }
+    }, []);
+    return (
+        <>
+            {children}
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme={isDarkMode ? 'dark' : 'light'}
+            />
+        </>
+    );
 }
 
 export default App;
