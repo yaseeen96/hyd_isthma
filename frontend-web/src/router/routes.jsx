9786382c262ba4bbb68d9343fcd6599ca390a20e
yaseeen96 <syedyaseen1539@gmail.com>
2024-06-10 10:23:04 +0530

    update routes

--- frontend-web/src/router/routes.jsx
+++ frontend-web/src/router/routes.jsx
@@ -1,25 +1,36 @@
 import { lazy } from 'react';
-import Otp from '../pages/Authentication/Otp';
+import HomeWrapper from '../pages/home/HomeWrapper';
+
+const RegisterPage = lazy(() => import('../pages/registration/registerPage'));
+const Otp = lazy(() => import('../pages/Authentication/Otp'));
 
 const Login = lazy(() => import('../pages/Authentication/Login'));
 const Index = lazy(() => import('../pages/Index'));
 
 const routes = [
-    // dashboard
     {
-        path: '/index',
-        element: <Index />,
+        path: '/',
+        layout: 'blank',
     },
     // Authentication
     {
-        // path: '/auth/login',
-        path: '/', // temp url
+        path: '/login',
         element: <Login />,
         layout: 'blank',
     },
     {
-        path: '/auth/verifyOtp',
+        path: '/verifyOtp',
         element: <Otp />,
+        layout: 'blank',
+    },
+    {
+        path: '/home',
+        element: <HomeWrapper />,
+        layout: 'blank',
+    },
+    {
+        path: '/home/register',
+        element: <RegisterPage />,
         layout: 'blank',
     },
 ];
