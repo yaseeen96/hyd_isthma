be3727c115c71eb439c4e3c25e7666ac14eae1b7
yaseeen96 <syedyaseen1539@gmail.com>
2024-06-10 10:26:03 +0530

    add a tile button component

---
+++ frontend-web/src/pages/home/components/tileButton.jsx
@@ -0,0 +1,14 @@
+const TileButton = ({ title, icon, onClick, isCompleted }) => {
+    return (
+        <button onClick={onClick} className="w-full">
+            <div className={`flex  items-center justify-start w-full h-full p-4  rounded-lg shadow-sm dark:bg-gray-800 border-gray-950`}>
+                <div className={`flex items-center justify-center w-16 h-16 ${isCompleted ? 'bg-green-100 dark:bg-green-400' : 'bg-yellow-100 dark:bg-yellow-700'} text-gray-200 rounded-lg `}>
+                    {icon}
+                </div>
+                <div className={`ml-4 text-lg font-semibold ${isCompleted ? 'text-gray-300 dark:text-gray-400' : 'text-gray-800 dark:text-gray-300'}`}>{title}</div>
+            </div>
+        </button>
+    );
+};
+
+export default TileButton;
