41840917c9267cccd330ccd0e6c9151b98b45c12
yaseeen96 <syedyaseen1539@gmail.com>
2024-06-10 10:25:41 +0530

    add API integration to login screens

--- frontend-web/src/pages/Authentication/Login.jsx
+++ frontend-web/src/pages/Authentication/Login.jsx
@@ -1,33 +1,57 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { setPageTitle } from '../../store/themeConfigSlice';
 import { useNavigate } from 'react-router-dom';
 import IconPhone from '../../components/Icon/IconPhone';
 import AuthLayout from './Layout/AuthLayout';
+import { toast } from 'react-toastify';
+import { sendOtpService } from '../../services/login_service';
+import { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { userStateAtom } from '../../store/atoms/userStateAtom';
 
 const Login = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(setPageTitle('Login'));
-    });
+    const [loading, setLoading] = useState(false);
+    const [phone, setPhone] = useState('');
     const navigate = useNavigate();
-    function submitForm() {
-        navigate('/auth/verifyOtp');
-    }
+    const setUserState = useSetRecoilState(userStateAtom);
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const response = await sendOtpService(phone);
+            console.log('response');
+            setUserState((prev) => ({ ...prev, phone: phone }));
+            toast.success(response.message);
+            navigate('/verifyOtp');
+        } catch (error) {
+            console.log('error from login.jsx');
+            console.log(error);
+            toast.error(`${error}`);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <AuthLayout title="Enter your phone number to login">
             <form className="space-y-5 dark:text-white" onSubmit={submitForm}>
-                <div>
-                    <label htmlFor="Phone">Phone</label>
+                <div className="mt-5">
                     <div className="relative text-white-dark">
-                        <input id="Phone" type="email" placeholder="Enter Phone Number" className="form-input ps-10 placeholder:text-white-dark" />
+                        <input
+                            id="Phone"
+                            type="tel"
+                            placeholder="Enter Phone Number"
+                            className="form-input ps-10 placeholder:text-white-dark"
+                            onChange={(e) => {
+                                setPhone(e.target.value);
+                            }}
+                        />
                         <span className="absolute start-4 top-1/2 -translate-y-1/2">
                             <IconPhone fill={true} />
                         </span>
                     </div>
                 </div>
-                <button type="submit" className="btn btn-primary !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]">
-                    Sign in
+                <button type="submit" className="btn btn-primary !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]" onClick={submitForm} disabled={loading}>
+                    {loading ? 'Sending OTP' : 'Sign in'}
                 </button>
             </form>
         </AuthLayout>
--- frontend-web/src/pages/Authentication/Otp.jsx
+++ frontend-web/src/pages/Authentication/Otp.jsx
@@ -1,30 +1,58 @@
-import { useDispatch } from 'react-redux';
-import { setPageTitle } from '../../store/themeConfigSlice';
-import { useEffect } from 'react';
 import AuthLayout from './Layout/AuthLayout';
 import IconPhone from '../../components/Icon/IconPhone';
 
-const Otp = () => {
-    const dispatch = useDispatch();
+import { useNavigate } from 'react-router-dom';
+import { useDarkMode } from '../../utils/hooks/useDarkMode';
+import { verifyOtpService } from '../../services/login_service';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { userStateAtom } from '../../store/atoms/userStateAtom';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
-    useEffect(() => {
-        dispatch(setPageTitle('Login'));
-    });
-    function submitForm() {}
+const Otp = () => {
+    const isDarkMode = useDarkMode();
+    console.log('isDarkMode in Otp:', isDarkMode);
+    const navigate = useNavigate();
+    const [otp, setOtp] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [userState, setUserState] = useRecoilState(userStateAtom);
+
+    async function submitForm(e) {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const response = await verifyOtpService(userState.phone, otp);
+            setUserState((prev) => ({ ...prev, token: response.data.token, name: response.data.name, halqa: response.data.halqa }));
+            toast.success("Yayyyy! You're logged in");
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('name', response.data.user.name);
+            navigate('/home');
+        } catch (error) {
+            toast.error(`${error}`);
+        } finally {
+            setLoading(false);
+        }
+    }
     return (
         <AuthLayout title="Otp has been sent to your registered phone number">
             <form className="space-y-5 dark:text-white" onSubmit={submitForm}>
                 <div>
-                    <label htmlFor="OTP">OTP</label>
                     <div className="relative text-white-dark">
-                        <input id="OTP" type="number" placeholder="Enter your otp" className="form-input ps-10 placeholder:text-white-dark" />
+                        <input
+                            id="OTP"
+                            type="number"
+                            placeholder="Enter your OTP"
+                            className="form-input ps-10 placeholder:text-white-dark"
+                            onChange={(e) => setOtp(e.target.value)}
+                            disabled={loading}
+                        />
                         <span className="absolute start-4 top-1/2 -translate-y-1/2">
                             <IconPhone fill={true} />
                         </span>
                     </div>
                 </div>
                 <button type="submit" className="btn btn-primary !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]">
-                    Verify
+                    {loading ? 'Signing In' : 'Verify'}
                 </button>
             </form>
         </AuthLayout>
