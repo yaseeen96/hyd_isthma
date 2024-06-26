6dbd4cf2c9bec1ea4c8848a88d5c44fa5635f6d9
yaseeen96 <syedyaseen1539@gmail.com>
2024-06-26 12:32:02 +0530

    add axios client

---
+++ frontend application/ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Yaseen . on 25/06/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
