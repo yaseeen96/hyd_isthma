e3105f39a6f965ac8d74b79f223e52b34bcbeb9d
yaseeen96 <syedyaseen1539@gmail.com>
2024-08-01 19:22:14 +0530

    add time and distance from venue

--- frontend-web/src/pages/registration/subpages/additionalDetailsRegistration.jsx
+++ frontend-web/src/pages/registration/subpages/additionalDetailsRegistration.jsx
@@ -62,11 +62,25 @@ const AdditionalDetailsRegistration = () => {
     };
 
     const transportOptions = {
-        BUS: ['Mg Bus Station, Imlibun, Gowliguda, Hyderabad', 'Jubilee Bus Station, Gandhi Nagar', 'L. B. Nagar, Hyderabad', 'Aramghar Bus Stop', 'Other'],
-        TRAIN: ['Secunderabad Railway Station', 'Kacheguda Railway Station', 'Lingampally Railway Station', 'Hyderabad Deccan (Nampally) Railway Station', 'Other'],
-        FLIGHT: ['Rajiv Gandhi International Airport, Shamshabad', 'Other'],
+        BUS: [
+            'Mg Bus Station, Imlibun, Gowliguda, Hyderabad (15 KM, 30 Mins from venue)',
+            'Jubilee Bus Station, Gandhi Nagar (30 KM, 55 Mins from venue)',
+            'L. B. Nagar, Hyderabad (16 KM, 30 Mins from venue)',
+            'Aramghar Bus Stop (10 KM, 16 Mins from venue)',
+            'Other',
+        ],
+        TRAIN: [
+            'Secunderabad Railway Station (22 KM, 50 Mins from venue)',
+            'Kacheguda Railway Station (15 KM, 35 Mins from venue)',
+            'Lingampally Railway Station (37 KM, 50 Mins from venue)',
+            'Hyderabad Deccan (Nampally) Railway Station (14 KM, 35 Mins from venue)',
+            'Other',
+        ],
+        FLIGHT: ['Rajiv Gandhi International Airport, Shamshabad (11 KM, 20 Mins from venue)', 'Other'],
         OTHER: ['Other'],
     };
+
+    console.log(transportOptions);
 
     const validationSchema = Yup.object().shape({
         hotel_required: Yup.string().required('Required'),
