1ea315f58e8ac24a323dbda6da5a6ed057efca3d
yaseeen96 <syedyaseen1539@gmail.com>
2024-08-11 13:23:23 +0530

    remove build subdirectory

--- frontend-web/vite.config.js
+++ frontend-web/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    pdfjsWorker: ['pdfjs-dist/build/pdf.worker.entry'],
-                },
-            },
-        },
-    },
 });
