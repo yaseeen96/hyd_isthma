77de0c9dfa0fdd033b9a233e018334ea6383848c
kahkashanshaik <kahkashan1507@gmail.com>
2024-11-03 15:35:56 +0530

    small commit

--- app/Http/Resources/SessionThemeListResource.php
+++ app/Http/Resources/SessionThemeListResource.php
@@ -23,9 +23,9 @@ class SessionThemeListResource extends JsonResource
         return [
             'id' => $this->id,
             'theme_name' => empty($lang) ? $this->theme_name : $this->{$lang . '_theme_name'},
-            'session_convener' => empty($lang) ? $this->convener : ($speaker_details ? $speaker_details->{$lang . '_name'} : ''),
-            'convener_bio' => empty($lang) ? '' : ($speaker_details->bio ? $speaker_details->{$lang . '_bio'} : ''),
-            'convener_image' => !empty($speaker_details) ? ($speaker_details->getMedia('speaker_image')->first() ? $speaker_details->getMedia('speaker_image')->first()->getUrl() : env('APP_URL').'/assets/img/no-image.png') : null,
+            'session_convener' => !empty($this->convener) ? ( empty($lang) ? $this->convener : ($speaker_details ? $speaker_details->{$lang . '_name'} : '') ) : '',
+            'convener_bio' => !empty($this->convener) ? (empty($lang) ? '' : ($speaker_details->bio ? $speaker_details->{$lang . '_bio'} : '') ) : '',
+            'convener_image' => !empty($speaker_details) && !empty($this->convener) ? ($speaker_details->getMedia('speaker_image')->first() ? $speaker_details->getMedia('speaker_image')->first()->getUrl() : env('APP_URL').'/assets/img/no-image.png') : null,
             'theme_type' => ucfirst($this->theme_type),
             'hall_name' => $this->hall_name,
             'datetime' => date('Y-m-d', strtotime($this->date)) . ' ' . Carbon::parse($this->from_time)->format('h:i A'). ' - ' . Carbon::parse($this->to_time)->format('h:i A'),
