cf929ed69d1dfe348e715628aef18c88a4839ca1
yaseeen96 <syedyaseen1539@gmail.com>
2024-11-11 10:23:42 +0530

    dont show button when program url is null

--- frontend-web/src/pages/programs/components/sessionCard.jsx
+++ frontend-web/src/pages/programs/components/sessionCard.jsx
@@ -124,7 +124,7 @@ const SessionCard = forwardRef(
                                                 </span>
 
                                                 {/* View Translation Button */}
-                                                {(program.status === 'In Progress' || program.status === 'Completed') && (
+                                                {(program.status === 'In Progress' || program.status === 'Completed') && program.url != null && (
                                                     <a href={program.url} target="_blank" rel="noopener noreferrer" className="block w-full">
                                                         <button className="bg-primary-500 text-white py-2 px-4 mt-2 rounded-lg font-semibold hover:bg-primary-dark transition duration-200 w-full flex items-center justify-center space-x-2">
                                                             <FiExternalLink />
