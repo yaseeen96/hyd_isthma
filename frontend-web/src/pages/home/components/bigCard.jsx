189bcc700c87fcd2f0225fd59160364b1d1f143e
yaseeen96 <syedyaseen1539@gmail.com>
2024-11-11 11:08:33 +0530

    update style of card

--- frontend-web/src/pages/home/components/bigCard.jsx
+++ frontend-web/src/pages/home/components/bigCard.jsx
@@ -14,11 +14,13 @@ const BigCard = ({ className, textClassName, icon, title, onSelect, isCentered, 
     return (
         <button onClick={handlePress} disabled={isDisabled} className="transform transition-all duration-300 ease-out active:scale-90 active:opacity-70">
             <div
-                className={`${className} shadow-lg flex flex-col ${isCentered ? 'justify-center' : 'justify-end'} items-center border-2 border-black rounded-lg p-12 
+                className={`${className} shadow-lg flex flex-col ${isCentered ? 'justify-center' : 'justify-end'} items-center border-2 border-black rounded-lg py-8 
                     ${isPressed ? 'scale-90 opacity-70' : ''}`}
             >
                 <div className="my-2">{icon}</div>
-                <h1 className={`${textClassName}`}>{title}</h1>
+                <h1 className={`${textClassName} text-center whitespace-pre leading-relaxed`} style={{ maxWidth: '100%' }}>
+                    {title}
+                </h1>
             </div>
         </button>
     );
